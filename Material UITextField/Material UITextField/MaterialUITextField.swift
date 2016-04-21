f4f5467a6c4b5b99c496fa33b03aae1fe57f5d98
jeantimex <jean.timex@gmail.com>
2016-04-20 23:13:34 -0700

    Implemented the label animation.

--- Material UITextField/Material UITextField/MaterialUITextField.swift
+++ Material UITextField/Material UITextField/MaterialUITextField.swift
@@ -12,6 +12,8 @@ import UIKit
 class MaterialUITextField: UITextField {
     
     var borderLayer: CAShapeLayer!
+    var labelLayer: CATextLayer!
+    var placeholderText: String?
     
     @IBInspectable
     var borderColor: UIColor = UIColor.clearColor() {
@@ -27,24 +29,120 @@ class MaterialUITextField: UITextField {
         }
     }
     
+    @IBInspectable
+    var labelColor: UIColor = UIColor.clearColor() {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+    
+    override var placeholder: String? {
+        didSet {
+            placeholderText = oldValue
+        }
+    }
+    
+    #if !TARGET_INTERFACE_BUILDER
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        registerNotifications()
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        registerNotifications()
+    }
+    #endif
+    
+    func registerNotifications() {
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(MaterialUITextField.onTextEndEditing), name: UITextFieldTextDidEndEditingNotification, object: self)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(MaterialUITextField.onTextBeginEditing), name: UITextFieldTextDidBeginEditingNotification, object: self)
+    }
+    
     override func layoutSubviews() {
         super.layoutSubviews()
-        layoutBorder()
+        setupLayer()
     }
     
     override func drawRect(rect: CGRect) {
         super.drawRect(rect)
         drawBorder()
+        drawLabel()
+    }
+    
+    func setupLayer() {
+        layer.masksToBounds = false
+        
+        setupBorder()
+        setupLabel()
+    }
+    
+    func onTextBeginEditing() {
+        placeholder = nil
+        
+        if labelLayer != nil {
+            let opacityAnimation = CABasicAnimation(keyPath: "opacity")
+            opacityAnimation.toValue = 1
+            
+            let positionAnimation = CABasicAnimation(keyPath: "position.y")
+            positionAnimation.toValue = -labelLayer.bounds.size.height / 2.0 + 5
+            
+            let group = CAAnimationGroup()
+            group.animations = [opacityAnimation, positionAnimation]
+            group.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut)
+            group.removedOnCompletion = false
+            group.fillMode = kCAFillModeForwards
+            group.duration = 0.2
+            
+            labelLayer.addAnimation(group, forKey: nil)
+        }
+    }
+    
+    func onTextEndEditing() {
+        placeholder = placeholderText
+        
+        if text?.characters.count == 0 {
+            let opacityAnimation = CABasicAnimation(keyPath: "opacity")
+            opacityAnimation.toValue = 0
+            
+            let positionAnimation = CABasicAnimation(keyPath: "position.y")
+            positionAnimation.toValue = 0
+            
+            let group = CAAnimationGroup()
+            group.animations = [opacityAnimation, positionAnimation]
+            group.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut)
+            group.removedOnCompletion = false
+            group.fillMode = kCAFillModeForwards
+            group.duration = 0.2
+            
+            labelLayer.addAnimation(group, forKey: nil)
+        }
     }
     
     //
     // Add border layer as sub layer
     //
-    func layoutBorder() {
+    func setupBorder() {
         if borderLayer == nil {
             borderLayer = CAShapeLayer()
             borderLayer.frame = layer.bounds
             layer.addSublayer(borderLayer)
+        }
+    }
+    
+    func setupLabel() {
+        if labelLayer == nil {
+            labelLayer = CATextLayer()
+            labelLayer.opacity = 0
+            labelLayer.frame = layer.bounds
+            labelLayer.string = placeholder
+            labelLayer.font = font
+            labelLayer.fontSize = (font?.pointSize)!
+            labelLayer.foregroundColor = labelColor.CGColor
+            labelLayer.wrapped = false
+            labelLayer.alignmentMode = kCAAlignmentLeft
+            labelLayer.contentsScale = UIScreen.mainScreen().scale
+            layer.addSublayer(labelLayer)
         }
     }
     
@@ -63,6 +161,15 @@ class MaterialUITextField: UITextField {
             borderLayer.lineWidth = borderWidth
             borderLayer.strokeColor = borderColor.CGColor
             borderLayer.path = path.CGPath
+        }
+    }
+    
+    //
+    // Redraw the label
+    //
+    func drawLabel() {
+        if labelLayer != nil {
+            labelLayer.foregroundColor = labelColor.CGColor
         }
     }
     
